ad2e59e19c684bfaad3fa35027faf1f4ae1bac45
josecond69 <josecond69@gmail.com>
2018-05-03 18:07:36 +0200

    Version 1.0

---
+++ clase.php
@@ -0,0 +1,177 @@
+<?php
+/**
+*Ficheiro coa definición da clase <b>Coche</b> e máis métodos
+*
+*Define a clase Coche xunto coas súas propiedades, constructor e métodos. 
+*
+* @package JCR
+* @license https://creativecommons.org/licenses/by-sa/4.0/legalcode Creative Commons 4.0 Attributioin ShareAlike
+* @author José Conde Rivero <josecond@hotmail.es>
+* @version Version 0.1.2
+*/
+
+/**
+*
+*Clase Coche
+*
+* @package JCR
+* @license https://creativecommons.org/licenses/by-sa/4.0/legalcode Creative Commons 4.0 Attributioin ShareAlike
+* @author José Conde Rivero <josecond@hotmail.es>
+* @version Version 0.1.2
+*/
+class Coche{
+	/**
+	* @var integer RODAS indica el número de rodas do coche.
+	*/
+	const RODAS = 4;
+
+	/**
+	* @var string $color indica o color do coche.
+	*/
+	public $color;
+
+	/**
+	* @var string $modelo indica o modelo do coche.
+	*/
+	private $modelo;
+	
+	/**
+	* Construtor da clase Coche.
+	*
+	* @param string $color color do coche.
+	* @param string $modelo modelo do coche.
+	* @return void
+	*/	
+	public function __construct ($color = "", $modelo = ""){
+		$this->color = $color;
+		$this->modelo = $modelo;
+	}
+	
+	/**
+	* Setter da propiedade color de Coche.
+	*
+	* @param string $color color do coche.
+	* @return void
+	*/	
+	public function setColor($color){
+		$this->color = $color;
+	}
+
+	/**
+	* Getter da propiedade color.
+	*
+	* @return string Devolve o color do coche.
+	*/	
+	public function getColor(){
+		return $this->color;
+	}
+
+	/**
+	* Setter da propiedade modelo de Coche.
+	*
+	* @param string $modelo modelo do coche.
+	* @return void
+	*/	
+	public function setModelo($modelo){
+		$this->modelo = $modelo;
+	}
+
+	/**
+	* Getter da propiedade modelo.
+	*
+	* @return string Devolve o modelo do coche.
+	*/	
+	public function getModelo(){
+		return $this->modelo;
+	}
+
+	/**
+	* Función que comproba se unha palabra é palindromo.
+	*
+	* @param string $palabra É a palabra que se vai a comprobar.
+	* @return boolean Devolve si a palabra é palídromo ou non.
+	*/	
+	public function ePalindromo ($palabra){
+		/**
+		* @var boolean $devolve Contén o valor a devolver.
+		*/
+		$devolve = false;
+		if ($palabra == strrev($palabra)){
+			$devolve = true;
+		}
+		return $devolve;
+	}
+
+	/**
+	* Función que comproba se un numero é primo.
+	*
+	* @param integer $numero É o número que se vai a comprobar.
+	* @return boolean Devolve si o número é primo ou non.
+	*/	
+	public function ePrimo ($numero){
+		/**
+		* @var boolean $devolve Contén o valor a devolver.
+		*/		
+		$devolve= true;
+		
+		/**
+		* @var integer $divisores Contén o numero de divisores atopados;
+		*/
+		$divisores = 0;
+		for ($i = 1; $i < $numero; $i++) {
+			if ($numero % $i == 0) {
+				$divisores++;
+			}
+		}
+		if ($divisores > 1) {
+			$devolve=false;
+		}
+		return $devolve;
+	}
+
+	/**
+	* Función que comproba se un número é maior que outro.
+	*
+	* @param integer $numero1 É o primeiro número que se vai a comprobar.
+	* @param integer $numero2 É o segundo número que se vai a comprobar.
+	* @return integer Devolve o número maior.
+	*/		
+	public function maior ($numero1 , $numero2){
+		/**
+		* @var boolean $devolve Contén o valor a devolver.
+		*/		
+		$devolve=$numero1;
+		if ($numero2 > $numero1){
+			$devolve=$numero2;
+		}
+		return $devolve;
+	}
+		/**
+	* Función que calcula una ecuación de primeiro grao tipo ax+b=0.
+	*
+	* @param integer $a É o valor de a.
+	* @param integer $b É o valor de b.
+	* @return double Devolve o resultada da ecuación.
+	*/	
+	public function ecuacion($a , $b ){
+	return (($b*-1)/$a);
+	}
+	
+	/**
+	* Función que comproba se un un ano é bisiesto.
+	*
+	* @param integer $ano É o ano que se vai a comprobar.
+	* @return boolean Devolve si o ano é bisiesto ou non.
+	*/		
+	public function eBisiesto($ano){ 
+		/**
+		* @var boolean $devolve Contén o valor a devolver.
+		*/		
+		$devolve=false;
+		if (($ano % 4 == 0) && (($ano % 100 != 0) || ($ano % 400 == 0))){
+			$devolve=true;
+		}
+		return $devolve;
+	}	
+}
+?>
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+/**
+*Ficheiro que mostra o funcionamento da clase <b>Coche</b> e os seus métodos.
+*
+*Mostra a clase Coche xunto coas súas propiedades, constructor e métodos. 
+*
+* @package JCR
+* @license https://creativecommons.org/licenses/by-sa/4.0/legalcode Creative Commons 4.0 Attributioin ShareAlike
+* @author José Conde Rivero <josecond@hotmail.es>
+* @version Version 0.1.2
+*/
+// Incluimos o  arquivo que contén a clase Coche clase.php
+include "clase.php";
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>DAW Tarefa 06</title>
+    </head>
+    <body>
+		<header><h1>DAW Tarefa 06</h1></header>
+		<section>
+			<?php
+			// Creamos un obxeto Coche e visualizámos as suas propiedades cos métodos getter.
+			$coche= new Coche("Azul","Opel Zafira");
+			echo "<b>Cor do coche:</b>".$coche->getColor();
+			echo " <b>Modelo do coche:</b>".$coche->getModelo()."<br /><br />";
+			
+			//Cos setter cambiamos os valores das propiedades
+			$coche->setColor("Bermello");
+			$coche->setModelo("Seat Toledo");
+			echo "<b>Cor do coche modificado :</b>".$coche->getColor();
+			echo " <b>Modelo do coche modificado:</b>".$coche->getModelo()."<br /><br />";
+
+			//Metodo palíndromo
+			$palabra="arenera";
+			if ($coche->ePalindromo($palabra)){
+			   echo "A palabra <b>'".$palabra."'</b> é palíndromo.<br /><br />";
+			}else{
+			   echo "A palabra <b>'".$palabra."'</b> no é palíndromo.<br /><br />";
+			}
+			$palabra="adios";
+			if ($coche->ePalindromo($palabra)){
+			   echo "A palabra <b>'".$palabra."'</b> é palíndromo.<br /><br />";
+			}else{
+			   echo "A palabra <b>'".$palabra."'</b> no é palíndromo.<br /><br />";
+			}		   
+			
+			//Método ePrimo
+			$numero="13";
+			if ($coche->ePrimo($numero)){
+			   echo "O número <b>'".$numero."'</b> é primo.<br /><br />";
+			}else{
+			   echo "O número <b>'".$numero."'</b> no é primo.<br /><br />";
+			}
+			$numero="12";
+			if ($coche->ePrimo($numero)){
+			   echo "O número <b>'".$numero."'</b> é primo.<br /><br />";
+			}else{
+			   echo "O número <b>'".$numero."'</b> non é primo.<br /><br />";
+			}
+			
+			//Método maior
+			echo "O número maior é o <b>'".$coche->maior(8,25)."'</b><br /><br />";
+			
+			//Método ePrimo
+			$ano="2016";
+			if ($coche->eBisiesto($ano)){
+			   echo "O ano <b>'".$ano."'</b> é bisiesto.<br /><br />";
+			}else{
+			   echo "O ano <b>'".$ano."'</b> non é bisiesto.<br /><br />";
+			}			
+
+			$ano="2018";
+			if ($coche->eBisiesto($ano)){
+			   echo "O ano <b>'".$ano."'</b> é bisiesto.<br /><br />";
+			}else{
+			   echo "O ano <b>'".$ano."'</b> non é bisiesto.<br /><br />";
+			}			
+			?>
+		</section>
+		<footer><h2>José Conde Rivero</h2></footer>
+    </body>
+</html>
